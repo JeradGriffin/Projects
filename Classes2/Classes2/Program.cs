e2104bc2bc02c7e5184cd174ce3ec7ad9896d5eb
JeradGriffin <jerad.griffin@gmail.com>
2014-11-05 13:11:37 -0500

    New projects

---
+++ Classes2/Classes2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ClassesAndObjects
+{
+	class Employee
+	{
+		public int Age { get; set; }
+		public string Name { get; set; }
+		public double Salary { get; set; }
+		public DateTime StartingDate { get; set; }
+		public string PhoneNumber { get; set; }
+	}
+
+	class Program 
+	{
+		public static void Main (string[] args)
+		{
+			Employee Dave = new Employee()
+			{
+				Age = 35,
+				Name = "David Smith",
+				Salary = 50000.00,
+				StartingDate = new DateTime(2012,7,10),
+				PhoneNumber = "617-555-1212"
+			};
+
+			Console.WriteLine ("Dave's age is {0} he started on {1} and makes {2}",
+				Dave.Age, Dave.StartingDate, Dave.Salary);
+
+		}
+	}
+}
---
+++ Objects/Objects/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ClassesAndObjects
+{
+	class Employee
+	{
+		public int Age { get; set; }
+		public string Name { get; set; }
+
+	}
+
+	class Program 
+	{
+		public static void Main (string[] args)
+		{
+			Employee Dave = new Employee();
+			Dave.Age = 35;
+			Console.WriteLine ("Dave's age is {0}", Dave.Age);
+
+		}
+	}
+}
